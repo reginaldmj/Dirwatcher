b424bd6ce9d3ff3e7b477fb82733bc8360f62dab
Reginald <reginald.jeffersonm@gmail.com>
2021-01-21 11:56:46 -0600

    working with create parser function

--- dirwatcher.py
+++ dirwatcher.py
@@ -19,7 +19,18 @@ def watch_directory(path, magic_string, extension, interval):
 
 
 def create_parser():
-    # Your code here
+    # needs....
+    """An argument that controls the polling interval
+    (instead of hard-coding it)"""
+    """An argument that specifics the "magic text" to search for"""
+    """An argument that filters what kind of file extension
+    to search within (i.e., .txt, .log)"""
+    """An argument to specify the
+    directory to watch (this directory may not yet exist!)"""
+
+    parser = argparse.ArgumentParser(
+        description='Checking for magic string and logging instances'
+    )
     return
 
 
